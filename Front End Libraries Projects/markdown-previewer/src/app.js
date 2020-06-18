f989321c4fc278855ab9cf6c4577f29520ee1f16
unknown <matt.haynes@AD.TOASTYHOME.NET>
2020-06-18 18:22:02 +1000

    Added header, created styling. Added logic for markdown previewer

--- Front End Libraries Projects/markdown-previewer/src/app.js
+++ Front End Libraries Projects/markdown-previewer/src/app.js
@@ -1,23 +1,30 @@
 import React from "react";
 import "./app.css";
+import logo from "./img/square-2673252_640.png";
+import marked from "marked";
+import githubMark from "./img/GitHub-Mark-64px.png";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.test = null;
+    this.state = {
+      input: "",
+    };
+
+    this.updateParent = this.updateParent.bind(this);
+  }
+
+  updateParent(e) {
+    this.setState({ input: e.target.value });
   }
 
   render() {
     return (
-      <div
-        id="App"
-        className="container-fluid min-vh-100"
-        style={{ backgroundColor: "pink" }}
-      >
+      <div id="App" className="container-fluid min-vh-100">
         <div id="wrapper min-vh-100" className="row">
           <Header />
-          <TextArea />
-          <PreviewArea />
+          <TextArea updateParent={this.updateParent} />
+          <PreviewArea html={this.state.input} />
         </div>
       </div>
     );
@@ -26,15 +33,25 @@ class App extends React.Component {
 
 function Header() {
   return (
-    <div
-      id="header-wrapper"
-      className="col-lg-12 d-flex justify-content-center"
-    >
-      <header className="col-lg-8 h-100">
-        <ul>
-          <li>Entry 1</li>
-          <li>Entry 2</li>
-        </ul>
+    <div id="header-wrapper" className="col-lg-12">
+      <header className="w-100 h-100 d-flex justify-content-center align-items-center pt-3 pl-2">
+        <a href="#">
+          <img
+            src={logo}
+            alt="Rhombus Logo"
+            height="64"
+            className="img-blend"
+          ></img>
+        </a>
+        <h1 className="m-auto">Markdown Previewer</h1>
+        <a href="https://github.com">
+          <img
+            src={githubMark}
+            alt="Github Logo"
+            height="64"
+            className="ml-auto animation-rotate"
+          ></img>
+        </a>
       </header>
     </div>
   );
@@ -43,15 +60,7 @@ function Header() {
 class TextArea extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      input: "",
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ input: event.target.value });
-    console.log(this.state.input);
+    this.handleChange = props.updateParent;
   }
 
   render() {
@@ -59,8 +68,7 @@ class TextArea extends React.Component {
       <section id="text-area" className="text-area col-lg-6 py-3">
         <textarea
           id="text-area-widget"
-          className="no-resize h-100 w-100"
-          value={this.state.input}
+          className="no-resize h-100 w-100 widget"
           onChange={this.handleChange}
         />
       </section>
@@ -71,25 +79,15 @@ class TextArea extends React.Component {
 class PreviewArea extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      input: "",
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ input: event.target.value });
-    console.log(this.state.input);
   }
 
   render() {
     return (
       <section id="preview-area" className="preview-area col-lg-6 py-3">
-        <textarea
+        <div
           id="preview-area-widget"
-          className="no-resize h-100 w-100"
-          value={this.state.input}
-          onChange={this.handleChange}
+          className="no-resize h-100 w-100 widget p-3 overflow-auto"
+          dangerouslySetInnerHTML={{ __html: marked(this.props.html) }}
         />
       </section>
     );
